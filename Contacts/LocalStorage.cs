db94a82a4e91b7a44f349bd6c9e8b4fbea7dbf0c
Ilya Burakov <speedwatson@gmail.com>
2018-03-15 22:17:03 +0300

    Local contacts storage implementation

---
+++ Contacts/LocalStorage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Contacts {
+
+    public class LocalStorage : IStorage {
+        private List<Contact> contacts = new List<Contact>();
+
+        public void Add(Contact newContact) => contacts.Add(newContact);
+        public List<Contact> FindAll(Predicate<Contact> match) => contacts.FindAll(match);
+    }
+
+}
