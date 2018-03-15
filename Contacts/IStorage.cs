a956019521f5dde763eca3140380eb9af968316b
Ilya Burakov <speedwatson@gmail.com>
2018-03-15 22:15:49 +0300

    Contacts storage interface

---
+++ Contacts/IStorage.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Contacts {
+    public interface IStorage {
+        void Add(Contact newContact);
+        List<Contact> FindAll(Predicate<Contact> match);
+    }
+}
