f8848771ddfe0a73bada7662725756b4f7583d39
Ilya Burakov <speedwatson@gmail.com>
2018-03-15 22:15:33 +0300

    Add UserRefusedException

---
+++ Contacts/Exceptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Contacts {
+
+    public class UserRefusedException : Exception {
+        public UserRefusedException() { }
+        public UserRefusedException(string message) : base(message) { }
+        public UserRefusedException(string message, Exception inner) : base(message, inner) { }
+    }
+
+}
