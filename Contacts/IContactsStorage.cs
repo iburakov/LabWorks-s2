f35fa40b1b9f7e585cf1e54238207451d350543f
iburakov <speedwatson@gmail.com>
2018-04-23 22:42:38 +0300

    Require storages' support of finding by new fields

--- Contacts/IContactsStorage.cs
+++ Contacts/IContactsStorage.cs
@@ -9,7 +9,11 @@ namespace Contacts {
         List<Contact> FindByFirstName(string substring);
         List<Contact> FindByLastName(string substring);
         List<Contact> FindByFullName(string substring);
+        List<Contact> FindByNickname(string substring);
         List<Contact> FindByEmail(string substring);
+        List<Contact> FindByMailer(string substring);
         List<Contact> FindByPhone(string substring);
+        List<Contact> FindByNote(string substring);
+        List<Contact> FindByBirthday(string birthday);
     }
 }
