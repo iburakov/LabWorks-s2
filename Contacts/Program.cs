18afa9ca4fc4595a43c4fb6ac8e47ec3a71e08d3
iburakov <speedwatson@gmail.com>
2018-04-23 22:45:10 +0300

    Add menu items for searching by new fields
    Also refactored string field substring search handlers to avoid copypasting as much as possible.

--- Contacts/Program.cs
+++ Contacts/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Contacts.CommandLine;
 
 namespace Contacts {
@@ -28,36 +29,36 @@ namespace Contacts {
             return mainMenu;
         }
 
+
+        private delegate List<Contact> ContactsFinder(string substring);
+        private const string searchResultsHeader = "Search results";
+
+        private static void FieldSearchHandler(string field, ContactsFinder finder) {
+            string substring = IO.GetString($"{field} substring: ");
+
+            IO.PrintContactList(
+                searchResultsHeader,
+                finder.Invoke(substring)
+            );
+        }
+
         private static Menu NewSearchMenu(IContactsStorage storage) {
             var searchMenu = new Menu("Search by:");
 
-            string searchResultsHeader = "Search results";
-
             searchMenu.AddItem(new MenuItem("First name", () => {
-                string substring = IO.GetString("First name substring: ");
-
-                IO.PrintContactList(
-                    searchResultsHeader,
-                    storage.FindByFirstName(substring)
-                );
+                FieldSearchHandler("First name", storage.FindByFirstName);
             }));
 
             searchMenu.AddItem(new MenuItem("Last name", () => {
-                string substring = IO.GetString("Last name substring: ");
-
-                IO.PrintContactList(
-                    searchResultsHeader,
-                    storage.FindByLastName(substring)
-                );
+                FieldSearchHandler("Last name", storage.FindByLastName);
             }));
-           
-            searchMenu.AddItem(new MenuItem("Full name", () => {
-                string substring = IO.GetString("Full name substring: ");
 
-                IO.PrintContactList(
-                    searchResultsHeader,
-                    storage.FindByFullName(substring)
-                );
+            searchMenu.AddItem(new MenuItem("Nickname", () => {
+                FieldSearchHandler("Nickname", storage.FindByNickname);
+            }));
+
+            searchMenu.AddItem(new MenuItem("Full name", () => {
+                FieldSearchHandler("Full name", storage.FindByFullName);
             }));
 
             searchMenu.AddItem(new MenuItem("Phone", () => {
@@ -70,11 +71,23 @@ namespace Contacts {
             }));
 
             searchMenu.AddItem(new MenuItem("Email", () => {
-                string substring = IO.GetString("Email substring: ");
+                FieldSearchHandler("Email", storage.FindByEmail);
+            }));
+
+            searchMenu.AddItem(new MenuItem("Mailer", () => {
+                FieldSearchHandler("Mailer", storage.FindByMailer);
+            }));
+
+            searchMenu.AddItem(new MenuItem("Note", () => {
+                FieldSearchHandler("Note", storage.FindByNote);
+            }));
+
+            searchMenu.AddItem(new MenuItem("Birthday", () => {
+                string birthday = IO.GetBirthday();
 
                 IO.PrintContactList(
                     searchResultsHeader,
-                    storage.FindByEmail(substring)
+                    storage.FindByBirthday(birthday)
                 );
             }));
 
